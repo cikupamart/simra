6f7b2ecabf96f07a0bf426faa784eccd3e4399e5
Lia Siti Sholihah <siti.sholihah@jerbeeindonesia.com>
2014-07-24 10:52:48 +0700

    test 1

---
+++ config/main-nav.php
@@ -0,0 +1,16 @@
+<a rel="group0">
+	<img src="img/icons/home.png" alt="Beranda"/>
+	Beranda
+</a>
+<a rel="group1">
+	<img src="img/icons/tables.png" alt="SISFO"/>
+	SISFO
+ </a>
+ <a rel="group2">
+	<img src="img/icons/statistik.png" alt="Statistik"/>
+	Statistik
+ </a>
+ <a rel="group3">
+	<img src="img/icons/download_s.png" alt="Download"/>
+	Download
+ </a>
